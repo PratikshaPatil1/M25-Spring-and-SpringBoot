da5a1bacb34d2a6277821efc8029cc1f3443a8c7
HEMANT <HEMANT@DESKTOP-PE00915>
2022-11-30 21:03:36 +0530

    First Commit

---
+++ Spring_AutoWired/src/main/java/org/tnsindia/springautowired/Human.java
@@ -0,0 +1,36 @@
+package org.tnsindia.springautowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Human {
+	
+	//Human is dependent on the Heart Class
+	//Dependency in terms of objects
+	private Heart heart;
+	public void pumping()
+	{
+		heart.pump();
+		System.out.println("Name of the animal: "+heart.getNameOfAnimal()+"\n"
+				  +"No.of Heart: "+heart.getNoOfHeart());
+	}
+	//Achieving DI using setters
+	@Autowired
+	@Qualifier("Crocodile")
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+	//Achieving using constructor
+	public Human(Heart heart) {
+		super();
+		this.heart = heart;
+	}
+	
+	public Human() {
+		super();
+		this.heart=heart;
+	}
+	
+	
+
+}
