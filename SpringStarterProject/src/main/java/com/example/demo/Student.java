378438694763a2482bdd5267f839f74ee8de8374
HEMANT <HEMANT@DESKTOP-PE00915>
2022-12-02 20:24:00 +0530

    First Commit

---
+++ SpringStarterProject/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+/*below annotation is used to denote that a class is a component
+ *  It allows spring to automatically detect our custom beans */
+ @Component
+public class Student {
+	
+	//private data members
+	private int rollno;
+	private String name;
+	
+	//setters and getters method to access private data members
+	/*public int getRollno() {
+		return rollno;
+	}*/
+	
+	//DI using Setters
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	/*public String getName() {
+		return name;
+	}*/
+	public void setName(String name) {
+		this.name = name;
+	}
+	//user defined method
+	public void print()
+	{
+		System.out.println("Welcome to M25 batch");
+	}
+	public Student() {
+		super();
+		System.out.println("Welcome to INDIA...!");
+
+	}
+	
+	
+	
+
+}
